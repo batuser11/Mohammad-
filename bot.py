7cde9694fc2794dfdde384647d1cfc08243fda75
batuser11 <167933883+batuser11@users.noreply.github.com>
2024-04-24 04:45:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,15 @@ BOT_TOKEN = config('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+def isMSg(message): 
+    return True
+
 @bot.message_handler(commands=["start","help"])
 def welcome(message):
-    bot.send-message(message.chat.id,"welcome to My Bot")
-    #answer all msg
-    def isMSg(message): 
-        return True
-    
-@bot.message_handler(func=isMSg)         # type: ignore
+    bot.send_message(message.chat.id, "welcome to My Bot")
+
+@bot.message_handler(func=isMSg)         
 def reply(message):
-        bot.reply_to(message,"that not command")
+    bot.reply_to(message, "that not command")
+
 bot.polling()
